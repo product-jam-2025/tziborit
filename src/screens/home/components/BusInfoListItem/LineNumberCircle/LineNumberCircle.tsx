a6f6d6a4969825c9ea9bc04ded868248ca17b37a
itaybo <itaybo@wix.com>
2024-12-21 19:49:56 +0200

    itay PR

--- src/screens/home/components/BusInfoListItem/LineNumberCircle/LineNumberCircle.tsx
+++ src/screens/home/components/BusInfoListItem/LineNumberCircle/LineNumberCircle.tsx
@@ -11,6 +11,7 @@ const LineNumberCircle = (props: LineNumberCircleProps) => {
             <span>{lineNumber}</span>
         </div>
     );
+    // todo: itay was here
 }
 
 export default LineNumberCircle;
