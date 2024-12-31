fee71b244c8f6608d1235cae290b53da8f342698
yardenross <yarden.ross@mail.huji.ac.il>
2024-12-31 12:15:11 +0200

    Added fonts and floating Reports square

---
+++ src/components/Reports.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Reports.css'; // Import the stylesheet
+
+
+const Reports: React.FC = () => {
+    return (
+        <div className="reports-container">
+            {/* You can add additional content or elements inside the square here */}
+            <p>Reports Content</p>
+        </div>
+    );
+}
+
+export default Reports;
